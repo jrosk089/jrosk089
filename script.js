aa2a3c14333c90f9bf703356e1bdd4b3017a6a0d
jrosk089 <jonroskilly@gmail.com>
2020-12-29 20:19:06 +0100

    Fixed javascript for skills pics, added links to project pics

--- script.js
+++ script.js
@@ -9,15 +9,33 @@ function myFunction() {
   };
 
   let one = document.getElementById('html');
-  let two = document.getElementById('test');
+  let two = document.getElementById('css');
+  let three = document.getElementById('js');
+  let four = document.getElementById('git');
+  let five = document.getElementById('trombone');
+
 
   function showTag(){
-    two.style.visibility = 'visible';
+    let tag = this.lastElementChild;
+    tag.style.visibility = 'visible';
   };
   
   function hideTag(){
-    two.style.visibility = 'hidden';
+    let tag = this.lastElementChild;
+    tag.style.visibility = 'hidden';
   }
 
   one.addEventListener('mouseover', showTag);
   one.addEventListener('mouseout', hideTag);
+
+  two.addEventListener('mouseover', showTag);
+  two.addEventListener('mouseout', hideTag);
+
+  three.addEventListener('mouseover', showTag);
+  three.addEventListener('mouseout', hideTag);
+
+  four.addEventListener('mouseover', showTag);
+  four.addEventListener('mouseout', hideTag);
+
+  five.addEventListener('mouseover', showTag);
+  five.addEventListener('mouseout', hideTag);
